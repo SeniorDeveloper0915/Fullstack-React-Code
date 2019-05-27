3e5a84c61d9ea1928f7e0c15b73fbd46cdd02679
SeniorDeveloper0915 <senior.web.developer.0915@outlook.com>
2019-05-27 03:56:33 -0700

    Update diagrams.xml

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
-require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
